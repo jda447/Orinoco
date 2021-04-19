775bc89e22f608145c070862a7958f2886ca8197
jda447 <jda447@outlook.com>
2021-04-20 00:46:19 +0100

    added array & classes JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,43 @@
-let startingBalance = 0;
-let costOfZurss = 499;
+let zurssCamera = {
+    _id: "5be1ed3f1c9d44000030b061",
+    name: "Zurss 50S",
+    price: 49900,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "vcam_1.jpg"
+};
 
-let totalCost = startingBalance * costOfZurss;
+let hirschCamera = {
+    _id: "5be1ef211c9d44000030b062",
+    name: "Hirsch 400DTS",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 309900,
+    imageUrl: "vcam_2.jpg"
+};
 
-console.log(totalCost);
+let franckCamera = {
+    _id: "5be9bc241c9d440000a730e7",
+    name: "Franck JS 105",
+    price: 209900,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "vcam_3.jpg"
+};
+
+let kurosCamera = {
+    _id: "5be9c4471c9d440000a730e8",
+    name: "Kuros TTS",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 159900,
+    imageUrl: "vcam_4.jpg"
+};
+
+let katatoneCamera = {
+    _id: "5be9c4c71c9d440000a730e9",
+    name: "Katatone",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 59900,
+    imageUrl: "vcam_5.jpg"
+};
+
+let cameraIds = ['5be1ed3f1c9d44000030b061', '5be1ef211c9d44000030b062', '5be9bc241c9d440000a730e7', 'be9c4471c9d440000a730e8', '5be9c4c71c9d440000a730e9'];
+
+console.log(cameraIds);
