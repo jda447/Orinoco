14ddf0da75ef1486cb92b9c7bc9be90e548dbea4
jda447 <jda447@outlook.com>
2021-04-20 01:27:02 +0100

    camera class & array

--- scripts.js
+++ scripts.js
@@ -11,8 +11,8 @@ class camera {
 let zurssCamera = new camera ('5be1ed3f1c9d44000030b061', 'Zurss 50S', '49900');
 let hirschCamera = new camera ('5be1ef211c9d44000030b062', 'Hirsch 400DTS', '309900');
 let franckCamera = new camera ('5be9bc241c9d440000a730e7', 'Franck JS 105', '209900');
-let zurssCamera = new camera ('5be9c4471c9d440000a730e8', 'Kuros TTS', '159900');
-let kurosCamera = new camera ('5be9c4c71c9d440000a730e9', 'Katatone', '59900');
+let kurosCamera = new camera ('5be9c4471c9d440000a730e8', 'Kuros TTS', '159900');
+let katatoneCamera = new camera ('5be9c4c71c9d440000a730e9', 'Katatone', '59900');
 
 
 //_id products array
