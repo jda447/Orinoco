31d2c71d33cd84bc53baa230d06844aa7c5d8fb0
jda447 <jda447@outlook.com>
2021-04-26 01:04:22 +0100

    zurssCamera object added to localStorage

--- scripts.js
+++ scripts.js
@@ -1,70 +1,21 @@
-//Cameras 
-let cameras = {
-    _id: "5be1ed3f1c9d44000030b061",
-    name: "Zurss 50S",
-    price: 49900
-};
+//Cameras
+let zurssCamera = [];
+const addZurssCamera = (ev) => {
+    ev.preventDefault();
+    let zurss = {
+        _id: "5be1ed3f1c9d44000030b061",
+        name: "Zurss 50S",
+        price: 49900,
+    }
+    zurssCamera.push(zurss);
+    console.warn('added', {zurssCamera});
 
-let cameras_serialized = JSON.stringify(cameras);
+    localStorage.setItem('ZurssCamera', JSON.stringify(zurssCamera));
+}
+document.addEventListener ('DOMContentLoaded', () => {
+document.getElementById ('zurssButton').addEventListener('click', addZurssCamera);
+});
 
-localStorage.setItem("cameras", cameras_serialized);
-
-let cameras_deserialized = JSON.parse(localStorage.getItem("cameras"));
-console.log(cameras_deserialized);
-
-let cameraHirsch = {
-    _id: "5be1ef211c9d44000030b062",
-    name: "Hirsch 400DTS",
-    price: 309900
-};
-
-let cameraHirsch_serialized = JSON.stringify(cameraHirsch);
-
-localStorage.setItem("cameraHirsch", cameraHirsch_serialized);
-
-let cameraHirsch_deserialized = JSON.parse(localStorage.getItem("cameraHirsch"));
-console.log(cameraHirsch_deserialized);
-
-let cameraFranck = {
-    _id: "5be9bc241c9d440000a730e7",
-    name: "Franck JS 105",
-    price: 209900
-};
-
-let cameraFranck_serialized = JSON.stringify(cameraFranck);
-
-localStorage.setItem("cameraFranck", cameraFranck_serialized);
-
-let cameraFranck_deserialized = JSON.parse(localStorage.getItem("cameraFranck"));
-console.log(cameraFranck_deserialized);
-
-let cameraKuros = {
-    _id: "5be9c4471c9d440000a730e8",
-    name: "Kuros TTS",
-    price: 159900
-};
-
-let cameraKuros_serialized = JSON.stringify(cameraKuros);
-
-localStorage.setItem("cameraKuros", cameraKuros_serialized);
-
-let cameraKuros_deserialized = JSON.parse(localStorage.getItem("cameraKuros"));
-console.log(cameraKuros_deserialized);
-
-let cameraKatatone = {
-    _id: "5be9c4c71c9d440000a730e9",
-    name: "Katatone",
-    price: 59900
-};
-
-let cameraKatatone_serialized = JSON.stringify(cameraKatatone);
-
-localStorage.setItem("cameraKatatone", cameraKatatone_serialized);
-
-let cameraKatatone_deserialized = JSON.parse(localStorage.getItem("cameraKatatone"));
-console.log(cameraKatatone_deserialized);
-
-//end of cameras
 
 //Submit form
 
