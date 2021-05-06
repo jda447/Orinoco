17236aca0a3c3769d65bdbdad55891408a808b09
jda447 <jda447@outlook.com>
2021-05-06 02:14:39 +0100

    zurss&hirsch can add to localStorage

--- scripts.js
+++ scripts.js
@@ -1,59 +1,3 @@
-//Camera localStorage log
-
-
-let cameras = {
-    _id:"5be1ed3f1c9d44000030b061",
-    name: "Zurss 50S",
-    price: 49900,
-}
-
-let zurssCamera = [];
-const addZurssCamera = (ev) => {
-    ev.preventDefault();
-    let camera = {
-        _id:"5be1ed3f1c9d44000030b061",
-        name: "Zurss 50S",
-        price: 49900,
-    }
-    zurssCamera.push(camera);
-
-    console.log('added', {zurssCamera});
-
-    localStorage.setItem('ZurssCamera', JSON.stringify(zurssCamera));
-}
-document.addEventListener ('DOMContentLoaded', () => {
-document.getElementById ('zurssButton').addEventListener('click', addZurssCamera);
-});
-
-//end of Camera localStorage log
-
-
-//Submit form localStorage log
-
-let userDetails = [];
-const addUserDetails = (ev) => {
-    ev.preventDefault();
-    let user = {
-        firstName: document.getElementById('firstName').value,
-        lastName: document.getElementById('lastName').value,
-        address: document.getElementById('address').value,
-        city: document.getElementById('city').value,
-        email: document.getElementById('email').value,
-    }
-    userDetails.push(user);
-    document.querySelector('form').reset();
-
-    console.log('added', {userDetails});
-
-    localStorage.setItem('CustomerDetails', JSON.stringify(userDetails));
-}
-document.addEventListener ('DOMContentLoaded', () => {
-document.getElementsByTagName ('submit').addEventListener('click', addUserDetails);
-});
-
-//end of Submit form localStorage log
-
-
 function validate() {
       
     if( document.myForm.firstName.value == "" ) {
@@ -104,4 +48,45 @@ function toggleCameras() {
     } else {
       x.style.display = "none";
     }
+}
+
+function myFunction() {
+    alert("Camera added to cart!");
   }
+
+
+let zurssCamera = [];
+const addZurssCamera = (ev) => {
+    ev.preventDefault();
+    let camera = {
+        _id:"5be1ed3f1c9d44000030b061",
+        name: "Zurss 50S",
+        price: 49900,
+    }
+    zurssCamera.push(camera);
+
+    console.log('added', {zurssCamera});
+
+    localStorage.setItem('Zurss 50S', JSON.stringify(zurssCamera));
+}
+document.addEventListener ('DOMContentLoaded', () => {
+document.getElementById ('zurssButton').addEventListener('click', addZurssCamera);
+});
+
+let hirschCamera = [];
+const addHirschCamera = (ev) => {
+    ev.preventDefault();
+    let camera = {
+        "_id": "5be1ef211c9d44000030b062",
+        "name": "Hirsch 400DTS",
+        "price": 309900,
+    }
+    hirschCamera.push(camera);
+
+    console.log('added', {hirschCamera});
+
+    localStorage.setItem('Hirsch 400DTS', JSON.stringify(hirschCamera));
+}
+document.addEventListener ('DOMContentLoaded', () => {
+document.getElementById ('hirschButton').addEventListener('click', addHirschCamera);
+});
