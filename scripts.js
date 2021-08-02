b644d896aa1c07a392e2ddc7ee7278b655c06ab9
jda447 <jda447@outlook.com>
2021-08-02 01:04:01 +0100

    cameras fetched in index.html no images

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let productsContainer = document.getElementById('products');
+const uri = 'https://orinoco-op.herokuapp.com/api/cameras';
+let formSubmit = document.getElementById('formSubmit');
+
+fetch (uri)
+    .then((response) => response.json())
+    .then((data) => createCards(data));
+
+
+function createCards(array) {
+    const container = document.getElementById('container');
+    const length = array.length;
+
+    for (let i=0; i<length; i++) {
+        const card = createCard(array[i]);
+        container.appendChild(card);
+    }
+}
+
+function createCard(obj) {
+    const card = document.createElement('section');
+
+    const name = document.createElement('heading');
+    const price = document.createElement('p');
+    const img = document.createElement('img');
+
+    card.classList.add('card');
+
+    name.innerHTML = obj.name;
+    price.innerHTML = obj.price;
+
+    img.setAttribute('src', obj.imageURL);
+
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(price);
+
+    return card;
+}
