ffe791c4957f576260fd0a9d6581bf1226d134ca
jda447 <jda447@outlook.com>
2021-08-18 00:18:39 +0100

    files renamed

---
+++ screens/HomeScreen.js
@@ -0,0 +1,29 @@
+const camera = {
+    render: () => {
+    const { products } = data;
+        return `
+        <ul class = "products">
+        ${products.map( 
+            ( product ) => `
+        <li>
+            <div class="product">
+                <a href="/#/product/1${product._id}">
+                    <img src="${product.image}" alt="${product.name}" />
+                    <div class="product-name">
+                    ${product.name}
+                    </div>
+                      <div class="price">
+                        ${product.price}
+                    </div>
+                    <div class="description">
+                      ${product.description}
+                    </div>
+                    </a>
+            </div>
+        </li>
+        `)}
+        `
+    }
+}
+
+export default camera;
