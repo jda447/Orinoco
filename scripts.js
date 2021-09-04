ef1523580115c9149d3f036a0d7508007fb62ced
jda447 <jda447@outlook.com>
2021-09-04 02:06:55 +0100

    images & text showing with T.Literals & ${dot.}

--- scripts.js
+++ scripts.js
@@ -1,85 +1,29 @@
-"use strict";
-//Strict mode makes it easier to write "secure" JavaScript.
-//In strict mode, any assignment to a non-writable property, a getter-only property, a non-existing property, a non-existing variable, or a non-existing object, will throw an error.
-
-let productsContainer = document.getElementById('products');
-const url = 'https://orinoco-op.herokuapp.com/api/cameras';
-
-const singleLink = './product.html?=_id';
-
-//./product.html?=_id
-
-
-
-fetch (url)
-    .then((response) => response.json())
-    .then((data) => createCards(data));
-
-
-function createCards(array) {
-    const container = document.getElementById('container');
-
-    for (let i in array) {
-        const card = createCard(array[i]);
-        container.appendChild(card);
+async function getCameras() {
+    const url = 'https://orinoco-op.herokuapp.com/api/cameras';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
     }
 }
 
-function createCard(obj) {
-    const card = document.createElement('section');
 
-    const name = document.createElement('cameraName');
-    const price = document.createElement('price');
-    const img = document.createElement('img');
-    const description = document.createElement('description');
+async function displayCameras() {
+    let cameras = await getCameras();
+    let html = '';
+    cameras.forEach(camera => {
+        let htmlSegment = `<div class="camera">
+        <img src="${camera.imageUrl}" >
+        <h2>${camera.name} ${camera.description} ${camera.price}</h2>
+        </div>`;
 
-    //const link = document.createElement('product');
+        html += htmlSegment;
+    });
 
-    //let anotherLink = './product.html?=_id' + obj._id;
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
 
-    //or
-    
-    //link.setAttribute('href', `${singleLink}${obj._id}`);
-
-    card.classList.add('card');
-
-    name.innerHTML = obj.name;
-    price.innerHTML = "$" + obj.price.toFixed(2) / 100;
-    description.innerHTML = obj.description;
-
-    img.setAttribute('src', obj.imageUrl);
-
-    name.style.fontSize = "1.6rem";
-    name.style.marginTop = "0.4rem";
-    name.style.marginLeft = "1rem";
-
-    price.style.fontSize = "1.2rem";
-    price.style.marginTop = "0.4rem";
-    price.style.marginLeft = "1rem";
-    price.style.fontWeight = "bold";
-
-    description.style.fontSize = "1rem";
-    description.style.marginTop = "-0.4rem";
-    description.style.marginBottom = "2rem";
-    description.style.marginLeft = "1rem";
-
-    img.style.padding = "0.4rem";
-
-    card.appendChild(img);
-    card.appendChild(name);
-    card.appendChild(price);
-    card.appendChild(description);
-
-    return card;
 }
 
-
-let hidden = false;
-function toggleCameras() {
-    hidden = !hidden;
-    if(hidden) {
-        document.getElementById('container').style.visibility = 'hidden';
-    } else {
-        document.getElementById('container').style.visibility = 'visible';
-    }
-}
+displayCameras();
