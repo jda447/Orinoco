9116df2ca79d2b5ff1622690cd1a1606a6023dbf
jda447 <jda447@outlook.com>
2021-09-16 00:10:29 +0100

    rearranged Tliterals

--- scripts.js
+++ scripts.js
@@ -14,13 +14,14 @@ async function displayCameras() {
     let html = '';
     cameras.forEach(camera => {
         let htmlSegment = `
-        <a href="product.html?id=${camera._id}"><div class="camera">
-        <div class="card img-fluid col-4 p-2 m-4">
-        <img src="${camera.imageUrl}" class="col">
-        <h4>${camera.name}</h4>
-        <span>${camera.description}</span> 
-        <p class="price">$${camera.price.toFixed(2)/100}</p>
-        </a>
+            <a href="product.html?id=${camera._id}">
+                <div class="camera">
+                <div class="card img-fluid col-4 p-2 m-4">
+                    <img src="${camera.imageUrl}" class="col">
+                    <h4>${camera.name}</h4>
+                    <span>${camera.description}</span> 
+                    <p class="price">$${camera.price.toFixed(2)/100}</p>
+            </a>
         </div>
         `;
 
