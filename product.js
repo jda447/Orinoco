a975105d4f77762779dcdfc8e7178d984b34f549
jda447 <jda447@outlook.com>
2021-09-16 01:06:00 +0100

    retrieved & parsed productId

--- product.js
+++ product.js
@@ -1,12 +1,8 @@
-"use strict";
-//Strict mode makes it easier to write "secure" JavaScript.
-//In strict mode, any assignment to a non-writable property, a getter-only property, a non-existing property, a non-existing variable, or a non-existing object, will throw an error.
 
-const name = document.getElementById('cameraName');
-const price = document.getElementById('price');
-const img = document.getElementById('img');
-const description = document.getElementById('description');
-
-const addToCart = document.getElementById('addToCart');
-let product = {};
+function getProductId() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+    return id;
+}
 
