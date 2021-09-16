148af11cef50c7f51cbfcfd7d8854b4de342e86f
jda447 <jda447@outlook.com>
2021-09-16 01:45:56 +0100

    URL search param fetch function

--- product.js
+++ product.js
@@ -6,3 +6,13 @@ function getProductId() {
     return id;
 }
 
+function getSingleProductId(id) {
+    fetch('https://orinoco-op.herokuapp.com/api/cameras/' + id)
+    .then(response => response.json())
+    .then(data => {
+      product = data;
+      console.log(product);
+      showProduct(data);
+    })
+    .catch(err => console.log(err))
+}
