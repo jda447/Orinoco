a342a365fae7fae03039dc247e499e776b8fd7f0
jda447 <jda447@outlook.com>
2021-10-01 23:54:20 +0100

    lStorage items show in cart page

--- frontend/cart.js
+++ frontend/cart.js
@@ -3,8 +3,12 @@ let cartItems = JSON.parse(localStorage.getItem('cart'));
 for (let i in cartItems) {
     console.log(cartItems[i]);
 
+    if (typeof(Storage) !== "undefined") {
+        document.getElementById("result").innerHTML = localStorage.getItem("cart");
+      } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+      }
     let tr = document.createElement('tr');
-
     let imageCart = document.createElement('img');
     let nameCart = document.createElement('p');
     let lensCart = document.createElement('td');
