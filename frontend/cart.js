8f24f2a81a5f9344108d6f635c4f637ab62e0c2b
jda447 <jda447@outlook.com>
2021-10-01 01:03:03 +0100

    cart.js variables added

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,6 +1,14 @@
 let cartItems = JSON.parse(localStorage.getItem('cart'));
 console.log(cartItems);
 
+let imageCart = document.createElement('img');
+let nameCart = document.createElement('p');
+let lensCart = document.createElement('td');
+let priceCart = document.createElement('td');
+let btnRemoveCart = document.createElement('td');
+let divName = document.createElement('td');
+let quantity = document.createElement('td');
+
 
 /*function showCartItems() {
 let cartItems = JSON.parse(localStorage.getItem('cart'));
