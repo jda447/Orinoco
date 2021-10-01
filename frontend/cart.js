fb439f3516e23bf3b580e5df5ae0cec37c726f3a
jda447 <jda447@outlook.com>
2021-10-01 01:11:32 +0100

    cartItems show in console in for loop

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,13 +1,19 @@
 let cartItems = JSON.parse(localStorage.getItem('cart'));
-console.log(cartItems);
 
-let imageCart = document.createElement('img');
-let nameCart = document.createElement('p');
-let lensCart = document.createElement('td');
-let priceCart = document.createElement('td');
-let btnRemoveCart = document.createElement('td');
-let divName = document.createElement('td');
-let quantity = document.createElement('td');
+for (let i in cartItems) {
+    console.log(cartItems[i]);
+
+    let tr = document.createElement('tr');
+
+    let imageCart = document.createElement('img');
+    let nameCart = document.createElement('p');
+    let lensCart = document.createElement('td');
+    let priceCart = document.createElement('td');
+    let btnRemoveCart = document.createElement('td');
+    let divNameCart = document.createElement('td');
+    let quantityCart = document.createElement('td');
+
+}
 
 
 /*function showCartItems() {
