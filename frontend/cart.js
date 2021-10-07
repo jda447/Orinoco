d2098a08d6a8be694c81146551936294d73029e4
jda447 <jda447@outlook.com>
2021-10-07 17:29:36 +0100

    cart td rearranged

--- frontend/cart.js
+++ frontend/cart.js
@@ -13,6 +13,7 @@ if (typeof(Storage) !== "cart") {
         imageUrl.style.height = '72%';
         imageUrl.style.width = '72%';
         imageUrl.style.marginTop = "0.5rem";
+        imageUrl.style.paddingLeft = "0.5rem";
 
         let cameraName = document.createElement('td');
         let name = cartItems[i].name;
@@ -27,18 +28,22 @@ if (typeof(Storage) !== "cart") {
         let cameraPrice = document.createElement('td');
         let price = cartItems[i].price.toFixed(2)/100;
         cameraPrice.innerHTML = `$` + price;
-        cameraPrice.style.fontSize = "1.2rem";
+        cameraPrice.style.fontWeight = "550";
+        cameraPrice.style.marginLeft = "2rem";
 
         tableHtml.appendChild(itemRow);
         itemRow.appendChild(imageUrl);
         itemRow.appendChild(cameraName);
         itemRow.appendChild(select);
         itemRow.appendChild(cameraPrice);
+        
+        let quantity = document.createElement('td');
+        quantity.innerHTML = `<input type="quantity-input" class="col-2" value ="${cartItems[i].quantity}" type="number" value="1">`;
+        itemRow.appendChild(quantity);
 
         let deleteBtn = document.createElement('td');
-        itemRow.appendChild(deleteBtn);
         deleteBtn.innerHTML = `<input type="button" value="Delete" onclick="deleteRow(this)">`;
-        
+        itemRow.appendChild(deleteBtn);
 
     }
 }
