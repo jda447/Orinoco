127bf91ec472ae39b5d8e20bb8b1dd18c969e42c
jda447 <jda447@outlook.com>
2021-10-13 22:06:37 +0100

    cartNumber sets in lStorage (not first click)

--- frontend/product.js
+++ frontend/product.js
@@ -80,6 +80,7 @@ addToCart.addEventListener('click', () => {
 
     cameraChoice.push(cameraAdded);
     localStorage.setItem('cart', JSON.stringify(cameraChoice));
+    localStorage.setItem('cartNumber', JSON.stringify(cartItems.length));
     alert('Camera added to cart!');
     
 })
