7229f8272dac8d2019b0ec5ce7a7174d24fc7f2d
jda447 <jda447@outlook.com>
2021-10-19 03:20:26 +0100

    first&lastName validation

--- frontend/cart.js
+++ frontend/cart.js
@@ -110,4 +110,60 @@ function deleteRow(r, i) {
     totalCartPrice();
 }
 
+
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const addressInput = document.getElementById('address');
+const cityInput = document.getElementById('city');
+const emailInput = document.getElementById('email');
+
+const formSubmit = document.getElementById('formSubmit');
+
+
+let userDetails = [];
+    const addUserDetails = (ev) => {
+    ev.preventDefault();
+    let user = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        address: document.getElementById('address').value,
+        city: document.getElementById('city').value,
+        email: document.getElementById('email').value,
+    }
+    userDetails.push(user);
+    document.querySelector('form').reset();
+
+    console.log('added', {userDetails});
+
+    localStorage.setItem('CustomerDetails', JSON.stringify(userDetails));
+}
+
+document.addEventListener ('DOMContentLoaded', () => {
+document.getElementById ('formSubmit').addEventListener('click', addUserDetails);
+});
+
+
+firstName.addEventListener('blur', () => {
+    const regName = /^[a-zA-Z]+$/;
+    if (!regName.test(firstName.value)) {
+      firstName.style.border = 'red solid 1px';
+      return false;
+    }
+    else {
+      firstName.style.border = 'green solid 1px';
+      isFirstNameValid = true;
+    }
+})
+
+lastName.addEventListener('blur', () => {
+    const regName = /^[a-zA-Z]+$/;
+    if (!regName.test(lastName.value)) {
+      lastName.style.border = 'red solid 1px';
+      return false;
+    }
+    else {
+      lastName.style.border = 'green solid 1px';
+      isLastNameValid = true;
+    }
+})
     
