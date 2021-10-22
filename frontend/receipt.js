f5fd304465635207370c3f3a3a260ca949399c03
jda447 <jda447@outlook.com>
2021-10-22 01:31:21 +0100

    total restyled

--- frontend/receipt.js
+++ frontend/receipt.js
@@ -1,5 +1,7 @@
 let totalOrderPrice = document.getElementById('orderTotal');
-totalOrderPrice.innerHTML = localStorage.getItem('total');
+let total = localStorage.getItem('total');
+totalOrderPrice.innerHTML = "Total: $" + total;
+totalOrderPrice.style.fontSize = "1.5rem";
 
 let totalOrderId = document.getElementById('orderId');
 totalOrderId.innerHTML = localStorage.getItem('orderId');
